02a462aa85f6fb0b54b658740f58598efb24d301
Pete Wall <pete.wall@grafana.com>
2026-04-24 14:35:46 -0500

    set content type
    Signed-off-by: Pete Wall <pete.wall@grafana.com>

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Prometheus-style metrics.
 
 import os
 import signal
-from prometheus_client import Gauge, generate_latest
+from prometheus_client import Gauge, generate_latest, CONTENT_TYPE_LATEST
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse, RedirectResponse
 import co2meter as co2
@@ -69,4 +69,4 @@ def get_metrics():
     read_data()
     # https://fastapi.tiangolo.com/reference/responses/#fastapi.responses.PlainTextResponse
     # https://github.com/prometheus/client_python/blob/147c9d1ac01ae0c36446d1dee79aae85aaf98a6e/prometheus_client/exposition.py#L233
-    return PlainTextResponse(content=generate_latest())
+    return PlainTextResponse(content=generate_latest(), media_type=CONTENT_TYPE_LATEST)
